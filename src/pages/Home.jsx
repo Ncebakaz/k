c7fcc25ac0516fa2a8ffdf65c95baaa9331eb6f8
Ncebakazi Somdaka <109283030+Ncebakaz@users.noreply.github.com>
2024-02-14 09:32:15 +0200

    changes to be commited

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Home(){
     return(
-        <div>
-            <h1>Home</h1>
+        <div className='home-container'>
+            <h1></h1>
+            <p></p>
+            <Link to='vans'>Find your van</Link>
         </div>
     )
 }
