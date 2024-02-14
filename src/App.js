9466e6ec709e718ae2c6d7ad07544f98f3797fd7
Ncebakazi Somdaka <109283030+Ncebakaz@users.noreply.github.com>
2024-02-14 09:23:53 +0200

    files added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
+import Home from './pages/Home'
+import About from './pages/About'
 
 export default function App() {
     return (
         <BrowserRouter>
+            <header>
+                <Link to="/">Willovale</Link>
+                <nav>
+                    <Link to='/about'>About</Link>
+                </nav>
+            </header>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 export default function About(){
     return(
-        <div>
-            <h1>About</h1>
+        <div className='about-page-container'>
+            <img src={isaphephe.jpg} className='about-hero-image'/>
+            <div className='about-page-content'>
+            <h1>willovale</h1>
+            <p>one</p>
+            <p>two</p>
+            </div>
+            <div className='about-page-cta'>
+                <h2>You driver is waiting.<br /> Your cab is ready</h2>
+                <Link className='link-button' to='/vans'>Explore our Cabs.</Link>
+            </div>
         </div>
     )
 }
