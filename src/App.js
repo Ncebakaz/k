cff7b9db64b14481931543391f5a77158f56dbad
Ncebakazi Somdaka <109283030+Ncebakaz@users.noreply.github.com>
2024-02-14 08:34:26 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route />
+                <Route path='/' element={ } />
             </Routes>
         </BrowserRouter>
     )
