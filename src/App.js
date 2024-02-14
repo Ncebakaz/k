b5aada26373ee6b6a8da5e808249c6e055ef5d97
Ncebakazi Somdaka <109283030+Ncebakaz@users.noreply.github.com>
2024-02-14 08:27:35 +0200

    added files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route />
+            </Routes>
+        </BrowserRouter>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
-
-function App() {
-    return (
-        <h1>Hello world!</h1>
-    )
-}
+import App from './App'
 
 
 ReactDom.createRoot(document.getElementById('root')).render(<App />);
